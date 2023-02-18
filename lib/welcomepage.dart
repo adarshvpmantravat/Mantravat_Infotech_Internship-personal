749bcd4ac87f4f3140e4cb28b87c6b60747a29a1
Adarsh V Patil <adarshvp.mantravat@gmail.com>
2023-02-18 08:29:31 +0530

    Save

--- lib/welcomepage.dart
+++ lib/welcomepage.dart
@@ -20,6 +20,43 @@ class _MyWidgetState extends State<MyWelcomePage> {
         ),
       ),
       drawer: const MainDrawer(),
+      body: Center(
+          child: Column(
+        children: [
+          const Padding(padding: EdgeInsets.all(25)),
+          const Image(
+              image: AssetImage(
+            'assets/Wall-e image picker.jpg',
+          )),
+          CustomButton(
+              title: 'Pick from Gallery',
+              icon: Icons.image_outlined,
+              onClick: () => {}),
+          CustomButton(
+              title: 'Open Camera',
+              icon: Icons.camera_alt_rounded,
+              onClick: () => {})
+        ],
+      )),
     );
   }
+}
+
+Widget CustomButton(
+    {required String title,
+    required IconData icon,
+    required VoidCallback onClick}) {
+  return Container(
+    width: 200,
+    child: ElevatedButton(
+      onPressed: onClick,
+      child: Row(children: [
+        Icon(icon),
+        const SizedBox(
+          width: 20,
+        ),
+        Text(title)
+      ]),
+    ),
+  );
 }
